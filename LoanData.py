4e01b9ef822ee331d344ecd7787497311e0b293e
kunal0x00 <117434806+kunal0x00@users.noreply.github.com>
2023-04-10 00:01:38 +0530

    Add files via upload
    you will receive .csv file after run this code, you can also edit this code to get own dataset

---
+++ LoanData.py
@@ -0,0 +1,14 @@
+import csv
+
+data = [
+    ['income', 'credit_score', 'loan_amount', 'employment_status', 'default', 'nth_amount', 'approved'],
+    [45000, 550, 8000, 'employed', 0, 18000.0, 1],
+    [55000, 620, 15000, 'employed', 0, 22000.0, 1],
+    [65000, 680, 25000, 'self-employed', 1, 26000.0, 0],
+    [75000, 720, 35000, 'employed', 0, 30000.0, 1],
+    [85000, 780, 45000, 'self-employed', 1, 34000.0, 0]
+]
+
+with open('loan_data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
